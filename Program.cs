d33074dbd2e87ecf3ce1491cc270e59b732388f2
Hycarus <mirkomarasco08@gmail.com>
2024-04-18 16:37:58 +0200

    super bonus done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace csharp_functions;
 
@@ -13,7 +14,7 @@ class Program
             Console.WriteLine("Sintassi errata. Insrisci un numero");
         }
         int[] array = new int[userNum];
-        for(int i = 0; i < array.Length; i++)
+        for (int i = 0; i < array.Length; i++)
         {
             Console.WriteLine($"Inserisci i numeri: {i + 1}/{array.Length}");
             while (int.TryParse(Console.ReadLine(), out array[i]) == false)
@@ -72,6 +73,98 @@ class Program
             }
             return sum;
         }
+
+
+
+
+
+
+        // ESERCIZIO AGGIUNTIVO DEL POMERIGGIO
+
+        double circleArea(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+        double userRadius;
+        Console.WriteLine("Inserisci il raggio per calcolare l'area del cerchio");
+        while (double.TryParse(Console.ReadLine(), out userRadius) == false)
+        {
+            Console.WriteLine("Sintassi errata. Inserisci un numero");
+        }
+        Console.WriteLine($"L'area del cerchio è: {circleArea(userRadius)}");
+
+
+
+        double convertCelsiusToFarenheit(double celsius)
+        {
+            double farenheit = (celsius * (9 / 5)) + 32;
+            return farenheit;
+        }
+        double celsius;
+        Console.WriteLine("Inserisci i gradi Celsius che vuoi convertire in Farenheit");
+        while (double.TryParse(Console.ReadLine(), out celsius) == false)
+        {
+            Console.WriteLine("Sintassi errata. Inserisci un numero");
+        }
+        Console.WriteLine($"{convertCelsiusToFarenheit(celsius)}");
+
+
+
+        string primeNumber(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return "non è un numero primo";
+            }
+            else
+            {
+                return "è un numero primo";
+            }
+        }
+        Console.WriteLine("Inserisci un numero per vedere se è un numero primo");
+        int num;
+        while (int.TryParse(Console.ReadLine(), out num) == false)
+        {
+            Console.WriteLine("Sintassi errata. Inserisci un numero valido");
+        }
+        Console.WriteLine($"Il numero che hai inserito {primeNumber(num)}");
+
+
+
+        string chaining(string a, string b, string character)
+        {
+            string chain = a + character + b;
+            return chain;
+        }
+        Console.WriteLine("Inserisci le due parole da concatenare");
+        Console.WriteLine("Parola 1");
+        string? word1 = Console.ReadLine();
+        Console.WriteLine("Parola 2");
+        string? word2 = Console.ReadLine();
+        Console.WriteLine("Inserisci un carattere per la concatenazione");
+        string? character = Console.ReadLine();
+        Console.WriteLine($"Ecco cosa viene fuori: {chaining(word1, word2, character)}");
+
+
+
+        string palindrome(string a)
+        {
+            char[] b = a.ToCharArray();
+            Array.Reverse(b);
+            string reverseWord = new string(b);
+            if (Equals(reverseWord, a))
+            {
+                return "La tua parola è palindroma";
+            }
+            else
+            {
+                return "La tua parola non è palindroma";
+            }
+        }
+        Console.WriteLine("Inserisci una parola per vedere se è palindroma");
+        string? word = Console.ReadLine();
+        Console.WriteLine(palindrome(word));
     }
 }
 
--- README.md
+++ README.md
@@ -22,3 +22,25 @@ Una volta completate queste funzioni di utilità di base, e dato il seguente arr
 
 **BONUS:** Convertire le funzioni appena dichiarate in funzioni generiche, ossia funzioni che possono lavorare con array di numeri interi **di lunghezza variabile**, ossia debbono poter funzionare sia che gli passi array di 5 elementi, sia di 6, di 7, di ... e così via.
 A questo punto modificare il programma in modo che chieda all’utente quanti numeri voglia inserire, e dopo di che questi vengono inseriti a mano dall’utente esternamente. Rieseguire il programma con l’input preso esternamente dall’utente.
+
+
+
+
+ESERCIZIO AGGIUNTIVO DEL POMERIGGIO
+
+
+
+Scrivere un piccolo programma che esegue le seguenti funzioni:
+
+(Si inserisca ogni funzionalità in uno o più metodi a seconda delle necessità)
+
+
+- Permette di Calcolare l'area di un cerchio  (I numeri sono in virgola quindi attenzione.)
+
+- Converta i gradi da Celsius a Farenheit
+
+- Verificare se il numero fornito in input è un numero primo o no
+
+- Concatenare due stringhe date in ingresso (BONUS: Permettere all'utente anche di scegliere il carattere per la concatenazione)
+
+- Verificare se una parola data in input è palindroma (HELP: la funzione Equals delle stringhe ci può aiutare)
